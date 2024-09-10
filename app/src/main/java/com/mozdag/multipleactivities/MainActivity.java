eb2dee6d961813b6fa80ac41a7f7cb81bf2881f4
m3mo <mehmetozdagg@gmail.com>
2024-09-11 01:49:50 +0200

    first

---
+++ app/src/main/java/com/mozdag/multipleactivities/MainActivity.java
@@ -0,0 +1,50 @@
+package com.mozdag.multipleactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText1;
+    String username;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        editText1 = findViewById(R.id.editText1);
+        username = "";
+    }
+
+    public void changeActivity(View view){
+
+        username = editText1.getText().toString();
+
+
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+
+        intent.putExtra("userInput",username);
+        startActivity(intent);
+
+
+    }
+
+
+}
